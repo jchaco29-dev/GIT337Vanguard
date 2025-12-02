a52cbe4cfea958dcd14268b99c6801d6cc588d01
jchaco29-dev <jchaco29@asu.edu>
2025-12-01 18:58:40 -0700

    GIT337 Vanguard

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobileNav.classList.toggle('active');
+    console.log("Hamburger clicked!"); // debug line
+});
+</script>
